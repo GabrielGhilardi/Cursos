12e8a7fc245d66e2522207947b020b682979537d
GabrielGhilardi <gabrielghilardi@gmail.com>
2021-06-24 01:29:20 -0300

    Aula 24/06
    Aula

---
+++ Curso Desenvolvimento Web/Javascript Fundamentos/obejto.js
@@ -0,0 +1,13 @@
+// Objeto é uma coleção chave valor
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4999.90
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'camisa Polo',
+    preco: 79.90,
+}
+
+console.log(prod2)
